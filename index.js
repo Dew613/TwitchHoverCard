cba5bc216154bffde17af434bde4d0b27cd263ef
Dew613 <dewansunnah613@gmail.com>
2024-01-03 16:46:33 +0000

    made the words stagger in

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 const subtitle = document.getElementsByClassName("card-subtitle")[0];
-console.log(subtitle)
-console.log("this works")
+// console.log(subtitle)
+// console.log("this works")
 //turn each word into a span and give it a class of "card-subtitle-word"
 const createWord = (text, index) => {
     const word = document.createElement("span");
     word.innerHTML = `${text} `;
 
     word.classList.add("card-subtitle-word");
+    word.style.transitionDelay = (`${index * 40}ms`);
     
     return word;
 }
@@ -23,5 +24,5 @@ const createSubtitleList = (text) => {
 }
 
 createSubtitleList("Day 2 of learning and figured out how to replicate this cool effect.")
-console.log("this works")
+// console.log("this works")
 
