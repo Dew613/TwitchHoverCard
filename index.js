259ecd973d8329a8794e43be199c8dbbcbb01bdd
Dew613 <dewansunnah613@gmail.com>
2024-01-03 16:34:01 +0000

    used java script to make text into spans for the subtitle

---
+++ index.js
@@ -0,0 +1,27 @@
+const subtitle = document.getElementsByClassName("card-subtitle")[0];
+console.log(subtitle)
+console.log("this works")
+//turn each word into a span and give it a class of "card-subtitle-word"
+const createWord = (text, index) => {
+    const word = document.createElement("span");
+    word.innerHTML = `${text} `;
+
+    word.classList.add("card-subtitle-word");
+    
+    return word;
+}
+
+
+// Make the given a list of words from the subtitle into the new class and add it to the subtitle element. 
+const addWord = (textlist, index) => {
+    subtitle.appendChild(createWord(textlist,index));
+}
+
+// turns the subtitle into a list and run it through addWord
+const createSubtitleList = (text) => {
+    text.split(" ").map(addWord);
+}
+
+createSubtitleList("Day 2 of learning and figured out how to replicate this cool effect.")
+console.log("this works")
+
